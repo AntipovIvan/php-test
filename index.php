5bdbb6fe2bd22a2d702fe5c5c33af98b36a447b0
AntipovIvan <85480386+AntipovIvan@users.noreply.github.com>
2023-03-17 14:46:37 +0900

    intelli

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Html;
 
 use Iterator;
@@ -7,22 +8,22 @@ use Iterator;
 
 // Classes/Objects START
 class Fruit {
-  public $name;
-  public $color;
+    public $name;
+    public $color;
 
-  function set_name ($name) {
-    $this->name = $name;
-  }
-  function get_name () {
-    return $this->name;
-  }
+    function set_name($name) {
+        $this->name = $name;
+    }
+    function get_name() {
+        return $this->name;
+    }
 
-  function set_color ($color) {
-    $this->color = $color;
-  }
-  function get_color () {
-    return $this->color;
-  }
+    function set_color($color) {
+        $this->color = $color;
+    }
+    function get_color() {
+        return $this->color;
+    }
 }
 
 $apple = new Fruit();
@@ -32,13 +33,13 @@ $apple->set_color("Red");
 $banana->set_name("Banana");
 $banana->set_color("Yellow");
 
-echo "Name: " .$apple->get_name();
+echo "Name: " . $apple->get_name();
 echo "<br>";
-echo "Color: " .$apple->get_color();
+echo "Color: " . $apple->get_color();
 echo "<br>";
-echo "Name: " .$banana->get_name();
+echo "Name: " . $banana->get_name();
 echo "<br>";
-echo "Color: " .$banana->get_color();
+echo "Color: " . $banana->get_color();
 echo "<br>";
 // Classes/Objects FINISH
 
@@ -48,24 +49,24 @@ echo "<br>";
 class Fruit2 {
     public $name;
     public $color;
-  
+
     function __construct($name, $color) {
         $this->name = $name;
         $this->color = $color;
     }
 
-    function get_name () {
+    function get_name() {
         return $this->name;
     }
-    function get_color () {
+    function get_color() {
         return $this->color;
     }
-  }
-  $apple = new Fruit2("Apple", "Red");
-  echo $apple->get_name();
-  echo "<br>";
-  echo $apple->get_color();
-  echo "<br>";
+}
+$apple = new Fruit2("Apple", "Red");
+echo $apple->get_name();
+echo "<br>";
+echo $apple->get_color();
+echo "<br>";
 // Constructors FINISH
 
 
@@ -94,18 +95,18 @@ class Fruit4 {
     protected $color;
     private $weight;
 
-    function set_name ($n) {
+    function set_name($n) {
         $this->name = $n;
     }
-    protected function set_color ($c) {
+    protected function set_color($c) {
         $this->color = $c;
     }
-    private function set_weight ($w) {
+    private function set_weight($w) {
         $this->weight = $w;
     }
 }
 
-$apple = new Fruit4 ();
+$apple = new Fruit4();
 $apple->set_name('Apple');
 echo "<br>";
 // $apple->set_color('Red');
@@ -119,11 +120,11 @@ echo "<br>";
 class Fruit5 {
     public $name;
     public $color;
-    public function __construct ($n, $c) {
+    public function __construct($n, $c) {
         $this->name = $n;
         $this->color = $c;
     }
-    protected function intro () {
+    protected function intro() {
         echo "The fruit is {$this->name} and the color is {$this->color}.";
     }
 }
@@ -137,7 +138,7 @@ final class Strawberry extends Fruit {
     public function intro() {
         echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight} gram.";
     }
-  }
+}
 $strawberry = new Strawberry("Strawberry", "red", 50);
 // $strawberry->message();
 $strawberry->intro();
@@ -149,14 +150,14 @@ echo "<br>";
 // Class constants START
 class Goodbye {
     const LEAVING_MESSAGE = "Thank you for visiting our site!";
-    public function byebye () {
+    public function byebye() {
         echo self::LEAVING_MESSAGE;
     }
 }
 echo Goodbye::LEAVING_MESSAGE;
 echo "<br>";
-$goodbye = new Goodbye ();
-$goodbye -> byebye ();
+$goodbye = new Goodbye();
+$goodbye->byebye();
 echo "<br>";
 // Class constants FINISH
 
@@ -165,10 +166,10 @@ echo "<br>";
 // Abstract classes START
 abstract class Car {
     public $name;
-    public function __construct ($n) {
+    public function __construct($n) {
         $this->name = $n;
     }
-    abstract public function intro () : string;
+    abstract public function intro(): string;
 }
 class Audi extends Car {
     public function intro(): string {
@@ -213,7 +214,7 @@ class ChildClass extends ParentClass {
         return "{$greet} {$prefix}{$separator} {$name}";
     }
 }
-  
+
 $class = new ChildClass;
 echo $class->prefixName("John Doe");
 echo "<br>";
@@ -225,27 +226,27 @@ echo "<br>";
 
 // Interfaces START
 interface Animal {
-    public function makeSound ();
+    public function makeSound();
 }
 class Cat implements Animal {
-    public function makeSound () {
+    public function makeSound() {
         echo "Meow";
     }
 }
 class Dog implements Animal {
-    public function makeSound () {
+    public function makeSound() {
         echo " Bark ";
     }
 }
 class Mouse implements Animal {
-    public function makeSound () {
+    public function makeSound() {
         echo " Squeak ";
     }
 }
-$cat = new Cat ();
-$dog = new Dog ();
-$mouse = new Mouse ();
-$animals = array ($cat, $dog, $mouse);
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
 foreach ($animals as $animal) {
     $animal->makeSound();
 }
@@ -256,12 +257,12 @@ echo "<br>";
 
 // Traits START
 trait message1 {
-    public function msg1 () {
+    public function msg1() {
         echo "OOP is fun!";
     }
 }
 trait message2 {
-    public function msg2 () {
+    public function msg2() {
         echo "OOP reduces code duplication!";
     }
 }
@@ -285,7 +286,7 @@ echo "<br>";
 
 // Static Methods START
 class greeting {
-    public static function welcome () {
+    public static function welcome() {
         echo "Hello World!";
     }
 }
@@ -293,10 +294,10 @@ greeting::welcome();
 echo "<br>";
 
 class greeting2 {
-    public static function welcome () {
+    public static function welcome() {
         echo "Hello World!";
     }
-    public function __construct () {
+    public function __construct() {
         self::welcome();
     }
 }
@@ -304,12 +305,12 @@ new greeting2();
 echo "<br>";
 
 class A {
-    public static function welcome () {
+    public static function welcome() {
         echo "Hello World!";
     }
 }
 class B {
-    public function message () {
+    public function message() {
         A::welcome();
     }
 }
@@ -318,13 +319,13 @@ echo $obj->message();
 echo "<br>";
 
 class domain {
-    protected static function getWebsiteName () {
+    protected static function getWebsiteName() {
         return "W3Schools.com";
     }
 }
 class domainW3 extends domain {
     public $websiteName;
-    public function __construct () {
+    public function __construct() {
         $this->websiteName = parent::getWebsiteName();
     }
 }
@@ -338,13 +339,13 @@ echo "<br>";
 // Static properties START
 class pi {
     public static $value = 3.14159;
-    public function staticValue () {
+    public function staticValue() {
         return self::$value;
     }
 }
 
 class x extends pi {
-    public function xStatic () {
+    public function xStatic() {
         return parent::$value;
     }
 }
@@ -369,7 +370,7 @@ echo "<br>";
 class Table {
     public $title = "";
     public $numRows = 0;
-    public function message () {
+    public function message() {
         echo "<p>Table '{$this->title}' has {$this->numRows} rows.</p>";
     }
 }
@@ -380,13 +381,15 @@ $table->numRows = 5;
 
 <!DOCTYPE html>
 <html>
-    <body>
+
+<body>
 
     <?php
-        $table->message();
+    $table->message();
     ?>
-        
-    </body>
+
+</body>
+
 </html>
 <!-- Namespaces FINISH -->
 
@@ -394,7 +397,7 @@ $table->numRows = 5;
 
 <?php
 // Iterables START
-function printIterable (iterable $myIterable) {
+function printIterable(iterable $myIterable) {
     foreach ($myIterable as $item) {
         echo $item;
     }
@@ -404,7 +407,7 @@ printIterable($arr);
 echo "<br>";
 
 
-function getIterable():iterable {
+function getIterable(): iterable {
     return ["a", "b", "c"];
 }
 $myIterable = getIterable();
@@ -418,26 +421,26 @@ class MyIterator implements Iterator {
     private $items = [];
     private $pointer = 0;
 
-    public function __construct ($i) {
+    public function __construct($i) {
         $this->items = array_values($i);
     }
-    public function current (): mixed {
+    public function current(): mixed {
         return $this->items[$this->pointer];
     }
-    public function key (): mixed {
+    public function key(): mixed {
         return $this->pointer;
     }
-    public function next (): void {
+    public function next(): void {
         $this->pointer++;
     }
-    public function rewind (): void {
+    public function rewind(): void {
         $this->pointer = 0;
     }
-    public function valid (): bool {
+    public function valid(): bool {
         return $this->pointer < count($this->items);
     }
 }
-function printIterable2 (iterable $myIterable) {
+function printIterable2(iterable $myIterable) {
     foreach ($myIterable as $item) {
         echo $item;
     }
@@ -450,132 +453,131 @@ printIterable2($iterator);
 
 
 <!-- PHP Form validation START -->
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
-    <head>
-        <style>
-            .error {color: #FF0000;}
-        </style>
-    </head>
-    <body>  
-        <?php
-            $nameErr = $emailErr = $genderErr = $websiteErr = "";
-            $name = $email = $gender = $comment = $website = "";
 
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                if (empty($_POST["name"])) {
-                    $nameErr = "Name is required";
-                } else {
-                    $name = test_input($_POST["name"]);
-                     // check if name only contains letters and whitespace
-                    if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                        $nameErr = "Only letters and white space allowed";
-                    }
-                }
-                  
-                if (empty($_POST["email"])) {
-                    $emailErr = "Email is required";
-                } else {
-                    $email = test_input($_POST["email"]);
-                    // check if e-mail address is well-formed
-                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                        $emailErr = "Invalid email format";
-                    }
-                }
-                    
-                if (empty($_POST["website"])) {
-                    $website = "";
-                } else {
-                    $website = test_input($_POST["website"]);
-                     // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-                    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
-                        $websiteErr = "Invalid URL";
-                    }
-                }
-                
-                if (empty($_POST["comment"])) {
-                    $comment = "";
-                } else {
-                    $comment = test_input($_POST["comment"]);
-                }
-                
-                if (empty($_POST["gender"])) {
-                    $genderErr = "Gender is required";
-                } else {
-                    $gender = test_input($_POST["gender"]);
-                }
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+    $name = $email = $gender = $comment = $website = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+                $nameErr = "Only letters and white space allowed";
             }
+        }
 
-            function test_input($data) {
-                $data = trim($data);
-                $data = stripslashes($data);
-                $data = htmlspecialchars($data);
-                return $data;
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
             }
-        ?>
+        }
 
-        <h2>
-            PHP Form Validation Example
-        </h2>
-        <p>
-            <span class="error">
-                * required field
-            </span>
-        </p>
+        if (empty($_POST["website"])) {
+            $website = "";
+        } else {
+            $website = test_input($_POST["website"]);
+            // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
+                $websiteErr = "Invalid URL";
+            }
+        }
 
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-            Name: <input type="text" name="name" value="<?php echo $name;?>">
-            <span class="error">
-                * <?php echo $nameErr;?>
-            </span>
-            <br><br>
+        if (empty($_POST["comment"])) {
+            $comment = "";
+        } else {
+            $comment = test_input($_POST["comment"]);
+        }
 
-            E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-            <span class="error">
-                * <?php echo $emailErr;?>
-            </span>
-            <br><br>
+        if (empty($_POST["gender"])) {
+            $genderErr = "Gender is required";
+        } else {
+            $gender = test_input($_POST["gender"]);
+        }
+    }
 
-            Website: <input type="text" name="website" value="<?php echo $website;?>">
-            <span class="error">
-                <?php echo $websiteErr;?>
-            </span>
-            <br><br>
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
 
-            Comment: <textarea name="comment" rows="5" cols="40">
-                <?php echo $comment;?>
+    <h2>
+        PHP Form Validation Example
+    </h2>
+    <p>
+        <span class="error">
+            * required field
+        </span>
+    </p>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        Name: <input type="text" name="name" value="<?php echo $name; ?>">
+        <span class="error">
+            * <?php echo $nameErr; ?>
+        </span>
+        <br><br>
+
+        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
+        <span class="error">
+            * <?php echo $emailErr; ?>
+        </span>
+        <br><br>
+
+        Website: <input type="text" name="website" value="<?php echo $website; ?>">
+        <span class="error">
+            <?php echo $websiteErr; ?>
+        </span>
+        <br><br>
+
+        Comment: <textarea name="comment" rows="5" cols="40">
+                <?php echo $comment; ?>
             </textarea>
-            <br><br>
+        <br><br>
 
-            Gender:
-            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?>
-                value="female">Female
-            <input type="radio" name="gender"<?php if (isset($gender) && $gender=="male") echo "checked";?>
-                value="male">Male
-            <input type="radio" name="gender"<?php if (isset($gender) && $gender=="other") echo "checked";?>
-                value="other">Other
-            <span class="error">* <?php echo $genderErr;?></span>
-            <br><br>
+        Gender:
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "female") echo "checked"; ?> value="female">Female
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "other") echo "checked"; ?> value="other">Other
+        <span class="error">* <?php echo $genderErr; ?></span>
+        <br><br>
 
-            <input type="submit" name="submit" value="Submit">  
-        </form>
+        <input type="submit" name="submit" value="Submit">
+    </form>
 
-        <?php
-            echo "Today is " . date("l");
-            echo "<h2>Your Input:</h2>";
-            echo $name;
-            echo "<br>";
-            echo $email;
-            echo "<br>";
-            echo $website;
-            echo "<br>";
-            echo $comment;
-            echo "<br>";
-            echo $gender;
-        ?>
-    </body>
+    <?php
+    echo "Today is " . date("l");
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+    ?>
+</body>
+
 </html>
 <!-- PHP Form validation FINISH -->
-
-
-
